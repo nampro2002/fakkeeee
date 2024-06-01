e0aaa12986a76ff91e862141d132ae3b503c1c56
nampro2002 <nampro2k2@gmail.com>
2024-06-01 12:31:04 +0700

    first commit

---
+++ src/main/java/vn/edu/fpt/SmartHealthC/SmartHealthCApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.SmartHealthC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHealthCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/fpt/SmartHealthC/SmartHealthCApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.SmartHealthC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
